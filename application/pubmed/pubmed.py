bfb0f7ee881c8f9eef44012388255bc547df9d67
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-10-21 17:55:29 -0400

    Add files via upload

--- application/pubmed/pubmed.py
+++ application/pubmed/pubmed.py
@@ -8,6 +8,7 @@ import collections
 import requests
 import shutil
 import random
+import json
 import os
 
 # Cool functions that are organized to look like a pip module file
@@ -60,13 +61,15 @@ class pubmed:
     fobj = open('./application/pubmed/results/results.txt', 'a', encoding='utf-8')
     alltexts = []
 
+    # summarize text
     for i in range(len(results)):
       try:
         dept2 = pubmed().getdocumentbyid(results[i][1])
         alltexts.append(dept2.abstract+' ')
       except TypeError:
         break
-
+    
+    # make word mesh
     img = Image.open('./application/pubmed/canvas.jpg')
     w, h = img.size
     draw = ImageDraw.Draw(img)
@@ -85,7 +88,7 @@ class pubmed:
       if word[1] >= 20: size += 20
 
       if word[0] not in stopwords.words:
-        draw.text((w/2+off+random.choice([-100,-50,0,50,100,150,200,250,300,350,400,450,500,550,600]), h/1.5+off+40),word[0],(random.randint(0,256), random.randint(0,256), random.randint(0,256)),font=ImageFont.truetype('./arial.ttf', size))
+        draw.text((w/2+off+random.choice([-100,-50,0,50,100,150,200,250,300,350,400,450,500,550,600]), h/1.5+off+40),word[0],(random.randint(0,256), random.randint(0,256), random.randint(0,256)),font=ImageFont.truetype('./application/pubmed/arial.ttf', size))
 
       i += 1
       off -= 30
@@ -98,3 +101,53 @@ class pubmed:
         fobj.write(dept2.abstract+'\n( Citation: '+dept2.doi+', Published on '+dept2.published+' )\n\n')
       except TypeError:
         break
+    
+    # make drug cloud
+    r = requests.get('https://www.drugs.com/imprints.php?drugname='+q.upper())
+    soup = BeautifulSoup(r.text, 'html.parser')
+    content = soup.find_all('div', { 'class': ['pid-img', 'pid-img-fit', 'zoom'] })
+    canvas = Image.open('./application/pubmed/canvas.jpg')
+    offset = {'x':10, 'y':10}
+
+    os.mkdir('./application/pubmed/results/images/')
+
+    for i in range(len(content)):
+        with requests.get(content[i].find('img')['src']) as request:
+            with open('./application/pubmed/results/images/item'+str(i)+'.png', 'wb') as f:
+                for chunk in request.iter_content(1024):
+                    f.write(chunk)
+        nobg = requests.post(
+            'https://sdk.photoroom.com/v1/segment',
+            files={'image_file': open('./application/pubmed/results/images/item'+str(i)+'.png', 'rb')},
+            headers={
+              'x-api-key': '0571bf20b9746d51c20b445fc2c66998de3f2dc1'
+            }
+        )
+        if nobg.ok:
+            with open('./application/pubmed/results/images/item'+str(i)+'.png', 'wb') as out:
+                out.write(nobg.content)
+        else:
+          print(nobg.status_code, nobg.content)
+        try:
+          canvas.paste(Image.open('./application/pubmed/results/images/item'+str(i)+'.png'), (offset['x'], offset['y']), Image.open('./application/pubmed/results/images/item'+str(i)+'.png'))
+        except ValueError:
+          canvas.paste(Image.open('./application/pubmed/results/images/item'+str(i)+'.png'), (offset['x'], offset['y']))
+        offset['x'] = offset['x'] + 200
+        offset['y'] = offset['y'] + random.choice([0,50,100,150,200,250,300])
+
+    canvas.save('./application/pubmed/results/drugcloud.png')
+    shutil.rmtree('./application/pubmed/results/images')
+
+    # get drug structures
+    r = requests.get("https://pubchem.ncbi.nlm.nih.gov/sdq/sdqagent.cgi?infmt=json&outfmt=json&query={%22download%22:%22*%22,%22collection%22:%22compound%22,%22where%22:{%22ands%22:[{%22*%22:%22aspirin%22}]},%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22PubChem_compound_text_aspirin%22}").text
+    headers = {
+      'referer': 'https://pubchem.ncbi.nlm.nih.gov/',
+      'user-agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.107 Safari/537.36'
+    }
+
+    soup = BeautifulSoup(requests.get('https://pubchem.ncbi.nlm.nih.gov/compound/'+json.loads(r)[0]['cid'], headers=headers).text, 'html.parser')
+    url = soup.find('meta', property='og:image')['content']
+    with open('./application/pubmed/results/structure.png', 'wb') as f:
+      with requests.get(url) as resp:
+        for chunk in resp.iter_content(1024):
+          f.write(chunk)
