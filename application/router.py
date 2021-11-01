67d366dc79be5358850fe1079ab24a3d435a5f33
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-11-01 07:59:12 -0400

    Update router.py

--- application/router.py
+++ application/router.py
@@ -27,14 +27,13 @@ def build():
       for folder, subfolders, filenames in os.walk('./application/pubmed/results'):
         for filename in filenames:
           zipobj.write(os.path.join(folder, filename), os.path.basename(os.path.join(folder, filename)))
-    send_file('./application/pubmed/results.zip')
-    return open('./application/static/html/contentpre.html')\
-      .read()\
-      .replace('||base1||', 'data:image/png;base64,'+base64.b64encode(open('./application/pubmed/results/cloud.png', 'rb').read()).decode('utf-8'))\
-      .replace('||base2||', 'data:image/png;base64,'+base64.b64encode(open('./application/pubmed/results/drugcloud.png', 'rb').read()).decode('utf-8'))\
-      .replace('||base3||', 'data:image/png;base64,'+base64.b64encode(open('./application/pubmed/results/structure.png', 'rb').read()).decode('utf-8'))
+    return send_file('./application/pubmed/results.zip')
   else:
     abort(404)
+
+@app.errorhandler(500)
+def internal(e):
+  return redirect('/')
 
 @app.url_value_preprocessor
 def after(endpoint, r):
